5b2d7689976e052171721fc338528af05563ff7d
diegok <diego@freekeylabs.com>
2011-09-04 18:37:34 +0200

    Auto deploy/update when schema use Schema::Versioned

--- lib/Mojolicious/Plugin/DBIC.pm
+++ lib/Mojolicious/Plugin/DBIC.pm
@@ -41,6 +41,7 @@ sub register {
     return ( values(%{$app->schema}) )[0];
 }
 
+# Load & connect a schema
 sub _init_schema {
     my ( $self, $schema_class, $options ) = @_;
 
@@ -57,27 +58,53 @@ sub _init_schema {
     eval "use $schema_class";
     if ($@) {
         Mojo::Log->debug("Can't use '$schema_class': $@");
+        #TODO: check if schema doesn't exists or exists but has errors.
         Mojo::Log->debug('Loading schema dynamically');
         eval {
             use DBIx::Class::Schema::Loader;
             DBIx::Class::Schema::Loader->naming('v7');
         };
         if ($@) {
-            Mojo::Log->error( "Can't load schema dynamically. It seems you need to install DBIx::Class::Schema::Loader: $@" );
-            die;
+            Mojo::Log->error( 
+              "Can't load schema dynamically. It seems you need to install DBIx::Class::Schema::Loader." 
+            );
+            die $@;
         }
         return DBIx::Class::Schema::Loader->connect(@conn_info);
     }
 
-    return $schema_class->connect(@conn_info);
+    Mojo::Log->debug("Connecting schema $schema_class");
+
+    $ENV{DBIC_NO_VERSION_CHECK}++;
+    $self->_deploy_schema($schema_class->connect(@conn_info));
+}
+
+# Deploy schema if it is deployable (via Schema::Versioned)
+sub _deploy_schema {
+    my ( $self, $schema ) = @_;
+    return $schema unless $schema->can('get_db_version');
+
+    if ( my $db_version = $schema->get_db_version ) {
+        unless ( $db_version eq $schema->schema_version ) {
+            Mojo::Log->debug("Upgrading schema from version $db_version to " . $schema->schema_version);
+            $schema->upgrade();
+        }
+    } 
+    else {
+        Mojo::Log->debug("Deploying schema version " . $schema->schema_version);
+        $schema->deploy();
+    }
+
+    $schema;
 }
 
 1;
+
 __END__
 
 =head1 NAME
 
-Mojolicious::Plugin::DBIC - Mojolicious Plugin
+Mojolicious::Plugin::DBIC - Mojolicious Plugin for DBIx::Class schemas
 
 =head1 SYNOPSIS
 
@@ -89,7 +116,14 @@ Mojolicious::Plugin::DBIC - Mojolicious Plugin
 
 =head1 DESCRIPTION
 
-L<Mojolicious::Plugin::DBIC> is a L<Mojolicious> plugin.
+L<Mojolicious::Plugin::DBIC> is a L<Mojolicious> plugin for using DBIx::Class
+schemas in your Mojolicious apps.
+
+This plugin will load an existing class or, if it doesn't exist it will try to
+load the schema using the optional dependency DBIx::Class::Schema::Loader.
+
+When schema exists and uses DBIx::Class::Schema::Versioned it will also check
+for the schema version and will try to update or deploy if it needs to.
 
 =head1 METHODS
 
